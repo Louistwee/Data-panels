0a256272327ae12822b9bc5322156ae4fb5b194d
Louistwee <louistwee@skynet.be>
2017-04-16 21:20:18 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,38 +1,14 @@
 (function(window,$,undefined){
-	
+	//connector object
 	$.connector = function(obj){
 		obj.connections = new Array();
-		obj.prototype = $.connector.fn;
+		obj.__proto__ = $.connector.fn;
 		return obj;
 	};
+	//__proto__ functions
 	$.connector.fn = {};
 	$.connector.fn.isConnectedWith = function(connector){
-	
-	};
-	$.fn.addConnector = function(obj){
-		//create the element
-		obj.connector = $('<div></div>')[0];
-		//add the input element
-		obj.element = $(this).get(0);
-		//create the connections array;
-		obj.connections = new Array();
-		
-		if(obj.type == 'output'){
-			$(obj.element).on('input',function(){
-				var value = this.value;
-				$.each(obj.connections,function(index,connection){
-					connection.change(value);
-				})
-			});
-			
-			
-		}else{
-			obj.change = function(value){
-				obj.element.value = value;
-			}
-		}
-		obj.isconnected = function(otherobj){
-			var checkconnection = false
+		var checkconnection = false
 			$.each(obj.connections,function(index,connection){
 				if(otherobj === connection){
 					if(checkconnection){
@@ -44,24 +20,53 @@
 					}
 				}
 			});
-		};
-		obj.connect = function(otherobj){
-			obj.connections.push(otherobj);
-			obj.isconnected(otherobj);
-			otherobj.connections.push(obj);
-			otherobj.isconnected(obj);
-		};
-		obj.disconnect = function(otherobj){
-			$.each(obj.connections,function(index,connection){
+	};
+	//change the output of #output
+	$.connector.fn.edit = function(value){
+		this.value = value;
+		$.each(this.connections,function(index,connection){
+			connection.change(value);
+		});
+	};
+	//fired by #output
+	$.connector.fn.change = function(value){
+		if(this.element){
+			this.element.value = value;
+		}
+	};
+	$.connector.fn.connect = function(otherobj){
+		this.connections.push(otherobj);
+		this.isconnected(otherobj);
+		otherobj.connections.push(this);
+		otherobj.isconnected(this);
+	};
+	$.connector.fn.disconnect = function(otherobj){
+		$.each(this.connections,function(index,connection){
 				if(otherobj === connection){
-					obj.connections.splice(index, 1);
+					this.connections.splice(index, 1);
 				}
 			});
 			$.each(otherobj.connections,function(index,connection){
-				if(obj === connection){
+				if(this === connection){
 					otherobj.connections.splice(index, 1);
 				}
 			});
+	}
+	$.getConnectorElement = function(){
+		//return a  div (little box) 5px x 5px with border 1px 
+	}
+	$.fn.addConnector = function(obj){
+		$.connector(obj);
+		//add the input element
+		obj.element = $(this).get(0);
+		//create the connections array;
+		
+		if(obj.type == 'output'){
+			$(obj.element).on('input',function(){
+				obj.edit(this.value)
+			});
+		}else{
+			
 		}
 		return obj;
 	}
