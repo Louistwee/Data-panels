32cfbd546ae1ef68176d71b261f890c1609c3206
Louistwee <louistwee@skynet.be>
2017-04-16 16:42:50 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,10 @@
-test
+(function(window,$,undefined){
+	$.fn.addConnector = function(obj){
+		obj.connector = $('<div></div>');
+		obj.element = $(this).get(0);
+		obj.connect = function(){
+			
+		};
+		return obj;
+	}
+})(window,$)
