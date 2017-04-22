a139d74d916c93fe85246f86e0905b482cbbd4e8
Louistwee <louistwee@skynet.be>
2017-04-22 19:15:17 +0200

    Update and rename Gradient.js to prefixCss.js

--- Files/Gradient.js
+++
@@ -1,4 +0,0 @@
-$.fn.cssGradient = function(gradient){
-	$(this).css('background','-webkit-'+gradient).css('background','-moz-'+gradient).css('background','-ms-'+gradient).css('background','-o-'+gradient).css('background',gradient);
-};
-//linear
