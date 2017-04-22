ba07b42629604563b3d54809cb8f0b3a270cbb12
Louistwee <louistwee@skynet.be>
2017-04-22 18:55:14 +0200

    Update Gradient.js

--- Files/Gradient.js
+++ Files/Gradient.js
@@ -4,7 +4,6 @@ $.fn.cssGradient = function(type,start,end){
 		.css('background','-moz-'+type+'-gradient('+start+', '+end+');')
 		.css('background','-ms-'+type+'-gradient('+start+', '+end+');')
 		.css('background','-o-'+type+'-gradient('+start+', '+end+');')
-		.css('background',type+'-gradient('+start+', '+end+');')
-	})
+		.css('background',type+'-gradient('+start+', '+end+');');
 };
 //linear
