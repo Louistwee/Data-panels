0e5c726602f14fb30efcc2fef273cd285e40566a
Louistwee <louistwee@skynet.be>
2017-05-15 17:09:49 +0200

    Update $.connect.panel.js

--- Files/$.connect.panel.js
+++ Files/$.connect.panel.js
@@ -1,3 +1,43 @@
-$.connect.Panel = function(settings){
-  return panel;
+$.connect.element = function(name,panelsettings){
+	var panel = document.createElement('div');
+	var panelLeft = document.createElement('div');
+	var panelRight = document.createElement('div');
+	var panelCenter = document.createElement('div');
+}
+$.connect.element = {};
+/*$.connect.panel.textarea = {
+	create:function(settings){
+		
+	},
+	type:'element',
+}*/
+$.connect.element.socket = {
+	inp:{
+		url:{
+			dataType:'string',
+			change:function(inp,element,self){
+				self.out.data.removeEvent(element.socket.onmessage);
+				element.socket.close();
+				element.socket = new WebSocket(inp);
+				self.out.data.addEvent(element.socket.onmessage);
+			},
+		},
+		data:{
+			dataType:'string',
+			change:function(inp,element,self){
+				
+			},
+		},
+	},
+	out:{
+		data:{
+			dataType:'string',
+			event:function(event,element,self){
+				return event.data;
+			},
+			value:'',
+		},
+		
+	},
+	type:'hidden',
 }
