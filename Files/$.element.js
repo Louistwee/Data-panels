0a64135b294fb0d46759b02b62efe8dd58a5492d
Louistwee <louistwee@skynet.be>
2017-05-18 17:58:48 +0200

    Update $.element.js

--- Files/$.element.js
+++ Files/$.element.js
@@ -1,6 +1,6 @@
 $.element = function(type,options){
 	var defaults = $.element[type];
-	var settings = $.extend( {}, defaults, options );
+	var settings = $.extend(true,{}, defaults, options );
 	if(settings.type === 'hidden'){
 		//creates the element
 		var element = $('div');
@@ -28,6 +28,7 @@ $.element = function(type,options){
 			inp.prepend(element.inp[i].boxPlace);
 			element.append(inp);
 		}
+		element.out =  {};
 		for(var i in settings.out){
 			$.connect({});
 			var out = $('div').css({padding:5,textAlign:'right'}).text(i);
@@ -38,6 +39,17 @@ $.element = function(type,options){
 			element.append(out);
 		}
 	};
+	//add the boxes
+	$.extend(true,element.inp,{type:'inp'},settings.inp);
+	for(var i in element.inp){
+		$.connect(element.inp[i]);
+		$(element.inp[i].boxPlace).replace($.connect.box(element.inp[i]));
+	}
+	$.extend(true,element.out,{type:'inp'},settings.out);
+	for(var i in element.out){
+		$.connect(element.out[i]);
+		$(element.out[i].boxPlace).replace($.connect.box(element.out[i]));
+	}
 	//append the element to the body element (if it exist), return it or append it to an other element;
 	if(settings.parent === 'return'){
 		return element;
@@ -56,7 +68,7 @@ $.element.socket = {
 	inp:{
 		url:{
 			dataType:'string',
-			change:function(inp,element,self){
+			change:function(inp,element){
 				if(element.socket) element.socket.close();
 				element.socket = new WebSocket(inp);
 				element.socket.onmessage = function(e){
@@ -67,7 +79,7 @@ $.element.socket = {
 		},
 		data:{
 			dataType:'string',
-			change:function(inp,element,self){
+			change:function(inp,element){
 				//send data to server
 			},
 			value:'',
