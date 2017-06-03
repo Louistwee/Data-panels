e4771c9b351e4d5b7414b650c921f3b1dbb0e8ac
Louistwee <louistwee@skynet.be>
2017-06-03 08:52:39 +0200

    Update $.element.js

--- Files/$.element.js
+++ Files/$.element.js
@@ -1,4 +1,4 @@
-$.element = function(options){
+$.element = function(options){ 
 	if(typeof options === 'string'){
 		var a = options;
 		options = {elementType:a};
@@ -10,11 +10,72 @@ $.element = function(options){
 	}
 	return element;
 }
-$.element.OpperationPannel = {};
-$.element.socket = {
+$.element.panel = {
+	info:'',
+	elementType:'panel',
+	create:function(options){
+		var element = $('<div>')[0];
+		var settings = $.extend(true,{},this,options);
+		$.extend(true,element,settings);
+		//css for the box
+		$(element).css({
+			boxShadow: '0px 0px 3px gray',
+			background:'white',
+			position: 'relative',
+			display:'inline-block',
+			width:150,
+			maxHeight: 300,
+			margin:10,
+			overflowX: 'hidden',
+			overflowY: 'auto',
+			fontSize:20,
+			fontFamily:'arial',
+			'-webkit-user-select': 'none',
+			'-moz-user-select': 'none',
+			'-ms-user-select': 'none',
+			'user-select': 'none', 
+		}).attr({title:settings.info}/*add a hover title*/).append($('<div/>').css({
+			padding:5,
+			fontWeight:'bold',
+			textAlign: 'center',
+		}).text(settings.elementType).drag(element));
+		return element;
+	},
+};
+$.element.opperationPannel = {
+	info:'',
+	elementType:'opperationPannel',
 	create:function(options){
 		var settings = $.extend(true,{},this,options);
 		var element = $.element.panel.create(settings);
+		//css for the box
+		for(var inputName in element.input){
+			var input = element.input[inputName];
+			input.element = element;
+			input.type = 'input';
+			$.connect(input);
+			input.div = $('<div/>').css({padding:5}).text(inputName);
+			var box = $.connect.box(input);
+			input.div.prepend(box);
+			$(element).append(input.div);
+		}
+		for(var outputName in element.output){
+			var output = element.output[outputName];
+			output.element = element;
+			output.type = 'output';
+			$.connect(output);
+			output.div = $('<div/>').css({padding:5,textAlign:'right'}).text(outputName);
+			var box = $.connect.box(output);
+			output.div.append(box);
+			$(element).append(output.div);
+		}
+		return element;
+	},
+};
+$.element.socket = {
+	create:function(options){
+		var settings = $.extend(true,{},this,options);
+		var element = $.element.opperationPannel.create(settings);
 		return element;
 	},
 	input:{
@@ -49,62 +110,10 @@ $.element.socket = {
 	info:'creates a websocket',
 	elementType:'Socket',
 };
-$.element.panel = {
-	info:'',
-	elementType:'Panel',
-	create:function(options){
-		var element = $('<div>')[0];
-		var settings = $.extend(true,{},this,options);
-		$.extend(true,element,settings);
-		//css for the box
-		$(element).css({
-			boxShadow: '0px 0px 3px gray',
-			background:'white',
-			position: 'relative',
-			display:'inline-block',
-			width:150,
-			maxHeight: 300,
-			margin:10,
-			overflowX: 'hidden',
-			overflowY: 'auto',
-			fontSize:20,
-			fontFamily:'arial',
-			'-webkit-user-select': 'none',
-			'-moz-user-select': 'none',
-			'-ms-user-select': 'none',
-			'user-select': 'none', 
-		}).attr({title:settings.info}/*add a hover title*/).append($('<div/>').css({
-			padding:5,
-			fontWeight:'bold',
-			textAlign: 'center',
-		}).text(settings.elementType).drag(element));
-		for(var inputName in element.input){
-			var input = element.input[inputName];
-			input.element = element;
-			input.type = 'input';
-			$.connect(input);
-			input.div = $('<div/>').css({padding:5}).text(inputName);
-			var box = $.connect.box(input);
-			input.div.prepend(box);
-			$(element).append(input.div);
-		}
-		for(var outputName in element.output){
-			var output = element.output[outputName];
-			output.element = element;
-			output.type = 'output';
-			$.connect(output);
-			output.div = $('<div/>').css({padding:5,textAlign:'right'}).text(outputName);
-			var box = $.connect.box(output);
-			output.div.append(box);
-			$(element).append(output.div);
-		}
-		return element;
-	},
-};
 $.element.parseJson = {
 	create:function(options){
 		var settings = $.extend(true,{},this,options);
-		var element = $.element.panel.create(settings);
+		var element = $.element.opperationPannel.create(settings);
 		return element;
 	},
 	input:{
@@ -130,7 +139,7 @@ $.element.parseJson = {
 $.element.console = {
 	create:function(options){
 		var settings = $.extend(true,{},this,options);
-		var element = $.element.panel.create(settings);
+		var element = $.element.opperationPannel.create(settings);
 		return element;
 	},
 	input:{
