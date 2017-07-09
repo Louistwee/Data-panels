6e7c2d59500b0ca9ef20c7f2053fb5d90d8b3fb8
Louistwee <louistwee@skynet.be>
2017-07-09 15:41:30 +0200

    Update $.loadscripts.js

--- Files/$.loadscripts.js
+++ Files/$.loadscripts.js
@@ -1,1 +1,27 @@
-
+var loadlist = {
+	connect:{
+		box:{},
+		line:{},
+	},
+	element:{
+		textInput:{},
+	},
+	dataTypes:{
+		
+	},
+	fn:{
+		drag:{},
+	},
+}
+$.getScriptList = function(list,parent){
+	if(list === {})return;
+	for(var i in list){
+		if(i === 'fn'){
+			$.getScriptList(list[i],parent + '.fn');
+		}else if(!(i in {})){
+			$.getScript(parent +'.'+ i + '.js');
+			$.getScriptList(list[i],parent + '.' + i);
+		}
+	}
+}
+$.getScriptList(loadlist,'../Files/$');
